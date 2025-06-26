706dd9537f9e2ec4f694adb8c3b1dfc42726a6cb
Matthew Bisnar <matthewbisnar@PHMBEMPT008.local>
2025-06-27 02:02:41 +0800

    added sort selector

--- src/components/ds/GameCard.tsx
+++ src/components/ds/GameCard.tsx
@@ -1,8 +1,9 @@
-import type { GameModel } from "@/hooks/useFetchGames"
 import { Card, HStack, Image } from "@chakra-ui/react"
 import PlatformIcons from "./PlatformIcons";
 import {} from 'react'
 import GameScore from "./GameScore";
+import type { GameModel } from "@/hooks/useGames";
+import { NoImage } from "@/configs/images";
 
 const GameCard = ({ game }: { game: GameModel}) => {
 
@@ -12,11 +13,13 @@ const GameCard = ({ game }: { game: GameModel}) => {
       border={`none`} 
       overflow={`hidden`}
     >
-        <Image src={game.background_image}/>
+        {game.background_image && (<Image src={game.background_image}/>)}
+        {!game.background_image && (<Image src={NoImage}/>)}
+
         <Card.Body gap="2">
             <Card.Title fontSize={`2xl`}>{game.name}</Card.Title>
             <HStack justifyContent={`space-between`}>
-              <PlatformIcons platforms={game.platforms.map(data => data.platform)}/>
+              <PlatformIcons platforms={game.platforms.map((data: any) => data.platform)}/>
               <GameScore score={game.metacritic}/>
             </HStack>
         </Card.Body>
--- src/configs/images.ts
+++ src/configs/images.ts
@@ -1,6 +1,8 @@
 import Logo from '@/assets/logo-ff4914e6.webp';
+import NoImage from '@/assets/no-image.jpg';
 
 
 export {
-    Logo
+    Logo,
+    NoImage
 }
